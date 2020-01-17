af471c3172ed81b55edb76c074e10dbd2bab1343
mianamirrashid <mian.amirrashid@gmail.com>
2020-01-17 17:35:10 -0500

    few more changes

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ console.log('Ready to Party With Some TypeScript!');
 
 
 const buttons = document.querySelectorAll('.button') as NodeListOf<HTMLButtonElement>;
+const billAmount = document.getElementById('billAmount') as HTMLTextAreaElement;
+const billAmount1 = document.getElementById('billAmount1') as HTMLTextAreaElement;
+const tipPercentage = document.getElementById('tipPercentage') as HTMLTextAreaElement;
+const tipAmount = document.getElementById('tipAmount') as HTMLTextAreaElement;
+const totalAmount = document.getElementById('totalAmount') as HTMLTextAreaElement;
 
 
 let currentbutton = 1;
@@ -21,8 +26,12 @@ function handleClick() {
 
     const that = this as HTMLButtonElement;
     that.className = 'clicked';
-    document.getElementById('lbpcnt').innerText = 'You are tipping '
-    + that.innerText ;
-    // that.removeEventListener('click', handleClick);
+    document.getElementById('lbpcnt').innerText = 'You are tipping ' + that.innerText ;
+    
+    billAmount1.value = 'Bill Amount: $' +  + billAmount.value;
+    
+    //'Bill Amount: $' + 
+    //document.getElementById('billAmount').innerText);
+    //document.getElementById('tipPercentage').value = 'Tip Percentage: ' + that.innerText;
 
 }
