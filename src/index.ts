1ff1e4bf79a6341520a80e13c93abb6c5e209236
mianamirrashid <mian.amirrashid@gmail.com>
2020-01-17 01:03:17 -0500

    updating first portion

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,26 @@
 import './styles.css';
 console.log('Ready to Party With Some TypeScript!');
 
+
+const buttons = document.querySelectorAll('.button') as NodeListOf<HTMLButtonElement>;
+
+
+let currentbutton = 1;
+buttons.forEach(bt => {
+    // bt.className = 'unclicked';
+    bt.addEventListener('click', handleClick);
+    currentbutton++;
+});
+
+function handleClick() {
+    buttons.forEach(btreset => {
+        btreset.className = 'unclicked';
+    });
+
+    const that = this as HTMLButtonElement;
+    that.className = 'clicked';
+    document.getElementById('lbpcnt').innerText = 'You are tipping '
+    + that.innerText ;
+    // that.removeEventListener('click', handleClick);
+
+}
