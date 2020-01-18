e097bed631ab15d56733edefe5af051777fe43e4
mianamirrashid <mian.amirrashid@gmail.com>
2020-01-17 22:46:02 -0500

    Optional Iteration 1 is done !!!!!

--- src/index.ts
+++ src/index.ts
@@ -13,25 +13,40 @@ const totalAmount = document.getElementById('totalAmount') as HTMLTextAreaElemen
 
 let currentbutton = 1;
 buttons.forEach(bt => {
-    // bt.className = 'unclicked';
-    
     bt.addEventListener('click', handleClick);
     currentbutton++;
 });
+
+function clickButtonAndUpdatefields(buttonId: string) {
+    const buttonClicked = document.getElementById(buttonId) as HTMLButtonElement;
+    buttonClicked.className = 'clicked';
+    localStorage.setItem('selectedButtonId', buttonId);
+    document.getElementById('lbpcnt').innerText = 'You are tipping ' + buttonClicked.innerText;
+    billAmount1.value = 'Bill Amount: $' + + checkValue(billAmount.value);
+    tipPercentage.value = 'Tip Percentage: ' + buttonClicked.innerText;
+    tipAmount.value = 'Amount of tip: $' + (checkValue(billAmount.value) * parseFloat(buttonClicked.innerText) * .01);
+    totalAmount.value = 'Total to be Paid: $' +
+        (checkValue(billAmount.value) + checkValue(billAmount.value) * parseFloat(buttonClicked.innerText) * .01);
+}
 
 function handleClick() {
     buttons.forEach(btreset => {
         btreset.className = 'unclicked';
     });
-
     const that = this as HTMLButtonElement;
-    that.className = 'clicked';
-    document.getElementById('lbpcnt').innerText = 'You are tipping ' + that.innerText ;
-    
-    billAmount1.value = 'Bill Amount: $' +  + billAmount.value;
-    
-    //'Bill Amount: $' + 
-    //document.getElementById('billAmount').innerText);
-    //document.getElementById('tipPercentage').value = 'Tip Percentage: ' + that.innerText;
-
+    clickButtonAndUpdatefields(that.id);
 }
+function checkValue(inputstring: string): number {
+    const x = parseFloat(inputstring);
+    if (isNaN(x) || x == null) { return 0; } else { return x; }
+}
+
+clickButtonAndUpdatefields(localStorage.getItem('selectedButtonId'));
+
+
+document.getElementById('billAmount').addEventListener('focusout', setDefaultButtonClick);
+
+function setDefaultButtonClick() {
+    clickButtonAndUpdatefields(localStorage.getItem('selectedButtonId'));
+}
+
